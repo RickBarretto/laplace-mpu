75392b45d2e97b2da5299c78710c968446c528b3
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2025-04-03 22:28:34 -0300

    use function and mat macro to access matrix's elements

--- mpu/operations/MpuDet.v
+++ mpu/operations/MpuDet.v
@@ -4,6 +4,7 @@
 `define MATRIX_5x5  0:(8*25-1)
 `define INTEGER_8   7:0
 `define at(row, col) ((row * (8 * 5)) + (col * 8))
+`define mat(row, col) matrix[`at(row, col) +: 8]
 `define atCol(col) (8 * col)
 
 
@@ -36,17 +37,45 @@ module MpuDet (
 
 	always @(posedge clock) begin
 		if (size == 1) result <= matrix[0 +: 8];
-		else if (size == 2) result <= 
-				matrix[0 +: 8] * matrix[`at(1, 1) +: 8] 
-				- matrix[`at(0, 1) +: 8] * matrix[`at(1, 0) +: 8];
-		else if (size == 3) result <= 
-				matrix[`at(0, 0) +: 8] * matrix[`at(1, 1) +: 8] * matrix[`at(2, 2) +: 8]
-				- matrix[`at(0, 1) +: 8] * matrix[`at(1, 2) +: 8] * matrix[`at(2, 0) +: 8]
-				+ matrix[`at(0, 2) +: 8] * matrix[`at(1, 0) +: 8] * matrix[`at(2, 1) +: 8];
-		else if (size == 4) result <= det4;
-		else if (size == 5) result <= det5;
-		else result <= 0;
+		else if (size == 2) 
+			result <= Det2(
+				`mat(0, 0), `mat(0, 1), 
+				`mat(1, 0), `mat(1, 1)
+			);
+		else if (size == 3) 
+			result <= Det3(
+				`mat(0, 0), `mat(0, 1), `mat(0, 2),
+				`mat(1, 0), `mat(1, 1), `mat(1, 2),
+				`mat(2, 0), `mat(2, 1), `mat(2, 2)
+			);
+		else if (size == 4) 
+			result <= det4;
+		else if (size == 5) 
+			result <= det5;
+		else 
+			result <= 0;
 	end
+
+	function [`INTEGER_8] Det2;
+		input [`INTEGER_8] a11, a12;
+		input [`INTEGER_8] a21, a22;
+
+		begin
+			Det2 = (a11 * a22) - (a12 * a21);
+		end
+	endfunction
+
+	function [`INTEGER_8] Det3;
+		input [`INTEGER_8] a11, a12, a13;
+		input [`INTEGER_8] a21, a22, a23;
+		input [`INTEGER_8] a31, a32, a33;
+
+		begin
+			Det3 = (a11 * a22 * a33) - (a13 * a22 * a31)
+				 + (a12 * a23 * a31) - (a12 * a21 * a33)
+				 + (a13 * a21 * a32) - (a11 * a23 * a32);
+		end
+	endfunction
 
 endmodule
 
